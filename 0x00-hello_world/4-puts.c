2c2d3f86ff07146613f80be906dcf7b6ea1d7520
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-16 14:50:44 -0800

    c 4th task 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * The puts function for the first time
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
