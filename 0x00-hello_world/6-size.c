c25728f60bdb07132a5fc66f44ab83538706f4fb
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-16 15:45:04 -0800

    c 6th task 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int lintType;
-	long long int llintType;
-	float floatType;
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
 
-	printf("Size of a char: %li byte(s)\n", (unsigned long)sizeof(charType));
-	printf("Size of an int: %li byte(s)\n", (unsigned long)sizeof(intType));
-	printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(lintType));
-	printf("Size of a long long int: %li byte(s)\n", (unsigned long)sizeof(llintType));
-	printf("Size of a float: %li byte(s)\n", (unsigned long)sizeof(floatType));
+	printf("Size of a char: %li byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %li byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %li byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %li byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
