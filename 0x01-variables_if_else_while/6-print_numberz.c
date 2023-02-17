5a5424fa45bc8da490995b726a2c9edad3989ce3
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-17 15:13:34 -0800

    c2 6th task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '1'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
