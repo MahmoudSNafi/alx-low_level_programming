8641303c63d40fb4964e18b6b6217c7706c31079
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-21 16:03:58 -0800

    nested 1st tak 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - function used to print letters
+ *
+ * print chars
  *
  * Return: alphabet in small caps
+ *
+ * no errors are expected
  */
 
 void print_alphabet(void)
 {
-	char c='a';
+	char c = 'a';
 
-	for (c='a';c<='z';c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
