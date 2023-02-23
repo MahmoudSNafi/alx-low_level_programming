0fb9c05857e2cb2b98c8ba768bbf11e8c50d8217
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-23 12:10:29 -0800

    more 7th task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing diagonal of slash character
+ * @n: input parameter
+ *
+ * Return: diagonal of slash
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+}
