99ff599562c46628f5130ec8853b3e5adc2eec68
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-23 00:02:16 -0800

    more 2nd task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of wto numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
