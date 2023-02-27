35cacc0a16b69c58086a547740369e8e2ef74158
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-27 09:24:39 -0800

    ptr 5th task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: input string
+ *
+ * Return: string reversed
+ */
+
+void rev_string(char *s)
+{
+	unsigned long int i;
+	char old_char;
+
+	for (i = 0; i <= strlen(s) / 2; i++)
+	{
+		old_char = s[i];
+		s[i] = s[strlen(s) - i - 1];
+		s[strlen(s) - i - 1] = old_char;
+	}
+}
