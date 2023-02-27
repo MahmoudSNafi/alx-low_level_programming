f67a820e3967da3b0031934dc44447d99907dd80
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-02-27 08:42:47 -0800

    ptr 2nd task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of string
+ * @s: input string
+ *
+ * Return: number as length
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
