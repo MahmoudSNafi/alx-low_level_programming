a187669f809e25e51eee4b129211899273d971f7
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-03-01 01:00:01 -0800

    more ptr 1st task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate n elements of an array into another
+ * @dest: destinationa array
+ * @src: source array
+ * @n: number of elements
+ *
+ * Return: concatenated array
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_ind = 0;
+	int dest_len = 0;
+	int i;
+
+	while (dest[dest_ind++])
+	{
+		dest_len++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	return (dest);
+}
