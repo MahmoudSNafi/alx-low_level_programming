c638ebc966d10b547f88964df0a3383684c901e4
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-03-10 11:32:00 -0800

    arg 3rd task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count of args
+ * @argv: vector of args
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	printf ("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
