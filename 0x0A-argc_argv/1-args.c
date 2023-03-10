7dfe8fba70d2d57b6ac251d61524b75748de91bd
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-03-10 11:15:05 -0800

    arg 1st task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
