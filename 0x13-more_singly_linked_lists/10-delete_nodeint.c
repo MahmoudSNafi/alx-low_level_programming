11da2fe3c0124b3def0c78c4db1198622a3471a7
MahmoudSNafi <mahmoud.s.nafi@gmail.com>
2023-04-05 15:20:29 -0700

    more lists 10th task 5

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tmp1);
 		return (1);
 	}
-	for (i = 0; i < (index -1); i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (tmp1->next == NULL)
 		{
